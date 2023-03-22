305dc41636f074b1d931dfcc7de8b79bd9a013e2
Paulo Gustavo Sousa Brito <oluap077@gmail.com>
2023-03-22 14:24:06 -0300

    métodos imprime com polimorfismo

--- modelo.py
+++ modelo.py
@@ -19,18 +19,28 @@ class Programa:
     def nome(self,novo_nome):
         self._nome=novo_nome.title() 
 
+    def imprime(self):
+        print(f'{self.nome} - {self.ano} - {self.likes}')
+
 class Filme(Programa):
     def __init__(self,nome,ano,duracao):
         super().__init__(nome,ano)
         self.duracao= duracao
+    def imprime(self):
+        print(f'{self.nome} - {self.ano} - {self.duracao} - {self.likes}')
 
 class Serie(Programa):
     def __init__(self,nome,ano,temporadas):
         super().__init__(nome,ano)
         self.temporadas= temporadas
+    def imprime(self):
+        print(f'{self.nome} - {self.ano} - {self.temporadas} - {self.likes}')
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao}')
 
 atlanta = Serie('atlanta ', 2018, 2)
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas}')
+
+filmes_e_series = [vingadores, atlanta]
+
+for programa in filmes_e_series:
+    programa.imprime()
